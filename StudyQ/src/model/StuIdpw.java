4b6c67ce62942b3502f3d3ced0789549cc1f58a7
dojo2021-a-kitani <kitani@seplus2016.onmicrosoft.com>
2021-06-10 13:37:27 +0900

    2021/06/10 ・daoフォルダ追加しました。 ・modelフォルダ追加しました。 ・servletフォルダ追加しました。

---
+++ StudyQ/src/model/StuIdpw.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.io.Serializable;
+
+public class StuIdpw implements Serializable {
+	private String s_id; //ID
+	private String s_pw; //PW
+
+
+}
---
+++ StudyQ/src/model/TeachIdpw.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.io.Serializable;
+
+public class TeachIdpw implements Serializable {
+	private String t_id; //ID
+	private String t_pw; //PW
+
+
+
+}
