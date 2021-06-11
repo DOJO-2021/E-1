5e71c71d8008b004c647844cfaf60b51774d8d06
dojo2021-a-kitani <kitani@seplus2016.onmicrosoft.com>
2021-06-12 01:04:41 +0900

    2021/06/12 以下のファイルを編集しました。 ・AppFile.java

--- StudyQ/src/servlet/AppFile.java
+++ StudyQ/src/servlet/AppFile.java
@@ -1,41 +1,34 @@
 package servlet;
-
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-/**
- * Servlet implementation class AppFile
- */
 @WebServlet("/AppFile")
+@MultipartConfig(location="/temp", maxFileSize=10000000)
 public class AppFile extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AppFile() {
-        super();
-        // TODO Auto-generated constructor stub
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Part part = request.getPart("file");
+        String name = this.getFileName(part);
+        part.write(getServletContext().getRealPath("/WEB-INF/appfile") + "/" + name);
+        response.sendRedirect("jsp/s_rsvResult.jsp");
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    private String getFileName(Part part) {
+        String name = null;
+        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+            if (dispotion.trim().startsWith("filename")) {
+                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+                name = name.substring(name.lastIndexOf("\\") + 1);
+                break;
+            }
+        }
+        return name;
+    }
 }
