254aee7bc88620dc9bd530b1390393c046573a96
dojo2021-a-kitani <kitani@seplus2016.onmicrosoft.com>
2021-06-11 17:45:07 +0900

    2021/06/11 以下のファイルを編集しました。 ・LoginUser.java ・StuIdpw.java ・TeachIdpw.java

--- StudyQ/src/model/StuIdpw.java
+++ StudyQ/src/model/StuIdpw.java
@@ -6,5 +6,30 @@ public class StuIdpw implements Serializable {
 	private String s_id; //ID
 	private String s_pw; //PW
 
+	public StuIdpw (String s_id, String s_pw) {
+		this.s_id = s_id;
+		this.s_pw = s_pw;
+	}
 
+	public StuIdpw() {
+		this.s_id = "";
+		this.s_pw = "";
+	}
+
+	public String getStuId() {
+		return s_id;
+	}
+
+	public void setStuId(String s_id) {
+		this.s_id = s_id;
+	}
+
+	public String getStuPw() {
+		return s_pw;
+	}
+
+	public void setStuPw(String s_pw) {
+		this.s_pw = s_pw;
+	}
 }
+
--- StudyQ/src/model/TeachIdpw.java
+++ StudyQ/src/model/TeachIdpw.java
@@ -6,6 +6,29 @@ public class TeachIdpw implements Serializable {
 	private String t_id; //ID
 	private String t_pw; //PW
 
+	public TeachIdpw (String t_id, String t_pw) {
+		this.t_id = t_id;
+		this.t_pw = t_pw;
+	}
 
+	public TeachIdpw() {
+		this.t_id = "";
+		this.t_pw = "";
+	}
 
+	public String getTeachId() {
+		return t_id;
+	}
+
+	public void setStuId(String t_id) {
+		this.t_id = t_id;
+	}
+
+	public String getTeachPw() {
+		return t_pw;
+	}
+
+	public void setTeachPw(String t_pw) {
+		this.t_pw = t_pw;
+	}
 }
