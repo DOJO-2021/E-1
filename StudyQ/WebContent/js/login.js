9aa978365804f084e894df3111a8ca912e2d0fef
dojo2021-a-kitani <kitani@seplus2016.onmicrosoft.com>
2021-06-18 09:52:46 +0900

    login.js

--- StudyQ/WebContent/js/login.js
+++ StudyQ/WebContent/js/login.js
@@ -1,14 +1,16 @@
 'use strict';
 
 document.getElementById('form').onsubmit = function(event) {
-event.preventDefault();
 const id = document.getElementById('form').s_id.value;
 const pw = document.getElementById('form').s_pw.value;
   if (id === "" && pw === "") {
+    event.preventDefault();
     document.getElementById('output').textContent = ('IDとPWを入力してください');
   } else if(id === "") {
+    event.preventDefault();
     document.getElementById('output').textContent = ('IDを入力してください');
   } else if(pw === "") {
+    event.preventDefault();
     document.getElementById('output').textContent = ('PWを入力してください');
   }
 }; /**
